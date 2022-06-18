667c900a3cb0a825cfbe2b43853b650aad9451a0
Minh Viet Pham <47558555+Izay0i@users.noreply.github.com>
2022-06-19 05:09:03 +0700

    fix: Fixed database URL

--- src/io/ecommerce/ConnectionConfigurator.java
+++ src/io/ecommerce/ConnectionConfigurator.java
@@ -3,5 +3,5 @@ package io.ecommerce;
 public interface ConnectionConfigurator {
     String username = "nqpfcsgp";
     String password = "YG9FLIylm_sXutv_TveF03IDHuznhgST";
-    String url = "jdbc:postgresql://posgres://";
+    String url = "jdbc:postgresql://tiny.db.elephantsql.com:5432/nqpfcsgp ";
 }
